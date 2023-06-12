96ecc2ac163fa4e64005ce2159da45cc6c628e5c
williamOnsare <65705419+williamOnsare@users.noreply.github.com>
2023-06-12 13:27:51 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# setup-chatsasa-widget
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-51y37k)
---
+++ script.js
@@ -0,0 +1,19 @@
+// The window.ChatSasa property gives access to methods for various operations, below is a demo
+
+if(window?.ChatSasa){
+  // To pass logged-in user info to the Widget whenever user signs in to your site
+  window.ChatSasa.setUserData({
+    user_id: "id-of-user", 
+    user_name: "name-of-user", 
+    user_email: "email-of-user"
+  });
+
+  // To remove logged-in user info from the Widget after user signs out on your site
+  window.ChatSasa.clearUserData();
+
+  // To open the widget
+  window.ChatSasa.open();
+
+  // To close the widget
+  window.ChatSasa.close();
+}
