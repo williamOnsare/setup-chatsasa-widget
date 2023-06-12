063a6f2a161b43e95b096b9bf035185bd69664b3
williamOnsare <65705419+williamOnsare@users.noreply.github.com>
2023-06-12 13:42:30 +0300

    adjust demo

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-// The window.ChatSasa property gives access to methods for various operations, below is a demo
+// The `window.ChatSasa` property gives access to methods for various operations, below is a demo
 
 if(window?.ChatSasa){
+  
   // To pass logged-in user info to the Widget whenever user signs in to your site
   window.ChatSasa.setUserData({
-    user_id: "id-of-user", 
-    user_name: "name-of-user", 
-    user_email: "email-of-user"
+    user_id: "id of user", 
+    user_name: "name of user", 
+    user_email: "email of user"
   });
 
   // To remove logged-in user info from the Widget after user signs out on your site
